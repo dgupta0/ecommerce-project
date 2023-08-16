798877b68fbb5f3bacc5ba3803951989b5c71047
Deeti <deetigupta8@gmail.com>
2023-08-16 20:21:55 +0530

    sec

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,8 @@ app.use(cors());
 app.use(express.json()); //this is used in place of body parser in the newer express version
 app.use("/admin", adminRoutes);
 app.use("/user", userRoutes);
+const mongoPass = "MerUMZDbEafdixt6"
 
-mongoose.connect('mongodb+srv://deetigupta8:MerUMZDbEafdixt6@cluster0.ewhiqzs.mongodb.net/ecom', { dbName: "ecom" });
+mongoose.connect(`mongodb+srv://deetigupta8:${mongoPass}@cluster0.ewhiqzs.mongodb.net/ecom`, { dbName: "ecom" });
 
 app.listen(3000, () => console.log('Server running on port 3000'))
